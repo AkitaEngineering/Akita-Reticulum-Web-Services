f6ccf923239c0ae1e88359b843566a64559c7a89
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-05-03 19:58:21 -0400

    Create __init__.py

---
+++ src/akita_reticulum_web_services/plugins/phoenix/__init__.py
@@ -0,0 +1,4 @@
+# src/akita_reticulum_web_services/plugins/phoenix/__init__.py
+
+# This file makes 'phoenix' a Python package within 'plugins'.
+# It allows browser plugins in this directory to be discovered and imported.
