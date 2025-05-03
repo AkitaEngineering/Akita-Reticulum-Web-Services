e63e81889d0e8ac727ab30e761322a4755419830
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-05-03 19:59:13 -0400

    Create __init__.py

---
+++ src/akita_reticulum_web_services/plugins/hexagon/__init__.py
@@ -0,0 +1,4 @@
+# src/akita_reticulum_web_services/plugins/hexagon/__init__.py
+
+# This file makes 'hexagon' a Python package within 'plugins'.
+# It allows server plugins in this directory to be discovered and imported.
